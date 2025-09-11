be4f6b3e1665d68af258f812bb5a4cc3c50de4de
LAPTOP-JPV850B8\mholz <if23b094@technikum-wien.at>
2025-09-11 08:45:57 +0200

    fixed docker and docker compose

--- PaperlessREST/obj/Debug/net8.0/PaperlessREST.AssemblyInfo.cs
+++ PaperlessREST/obj/Debug/net8.0/PaperlessREST.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaperlessREST")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+747de0502107fc411b6aaff5f9537c2f30cdb860")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaperlessREST")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaperlessREST")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
