3942e1c3a783c5d93d39a8121a4e7343aad24d81
Daniel Gebhart <daniel.gebhart@hotmail.com>
2025-09-25 10:56:57 +0200

    added first api functionality in UI layer

--- PaperlessUI/src/app/app.component.ts
+++ PaperlessUI/src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, JsonPipe],
+  template: `
+    <h1>PaperlessUI</h1>
+    <button (click)="testApi()">Test API</button>
+    <pre>{{ result | json }}</pre>
+  `,
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'PaperlessUI';
+export class AppComponent implements OnInit {
+  private http = inject(HttpClient);
+  result: unknown = {};
+
+  ngOnInit(): void {
+    this.result = {};
+  }
+
+  testApi(): void {
+    this.http.get('/api/documents').subscribe({
+      next: (res) => this.result = res,
+      error: (err) => this.result = err?.message ?? err
+    });
+  }
 }
